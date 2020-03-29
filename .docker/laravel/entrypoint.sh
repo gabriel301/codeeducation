dfa2d53fcb85c6b2bbb4167ad45436e41642b041
gabriel301 <gabriel301@gmail.com>
2020-03-29 19:22:55 -0300

    Incluindo dockerfile para imagem otimizada (PRD Ready)

--- .docker/laravel/entrypoint.sh
+++ .docker/laravel/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 dockerize --template /.env-template:/var/www/.env
 dockerize -wait tcp://db:3306 -timeout 40s
-php artisan key:generate
-php artisan migrate
+composer install && \
+php artisan key:generate && \
+php artisan migrate && \
 php-fpm
